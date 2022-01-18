b5d5bacb0a07092b398abc75a8e673d569c44d15
Nhat Vo <tonystark0243@gmail.com>
2022-01-18 17:50:50 +0100

    updated marketData
    updated file accordingly to Phuong's version

--- binancess/marketData.py
+++ binancess/marketData.py
@@ -1,37 +1,65 @@
 import requests
+import json
+import os.path
+from requests.models import REDIRECT_STATI
+from toolss import jsonProcess
 import time, datetime
-END_POINT = "https://api.binance.us"
+END_POINT = "https://api.binance.com"
 class marketData: 
-    def __init__(self, symbol,eSymbol):
+    def __init__(self, symbol,eSymbol = "USDT"):
         """ 
+        Source of Data for exchanging rate between a couple of assets(cryto and forex). If there is only one parameter, it is assumed that this will be converted to USD.
         :param symbol: The symbol of Cryto to be converted
-        :param eSymbol: The symbol of Cryto convert to
+        :param eSymbol: The symbol of Cryto convert to (default: USD)
         """
         self.symbol = symbol
         self.eSymbol = eSymbol
-   
-    def getCandlesticks(self, interval,startDate, endDate="now"):
+        self.dataFile = symbol + eSymbol
+
+
+    def getCandlesticksWithLimit1000(self, interval,startDate, endDate="now"):
         """
-        Get Candles Data In An Specific Interval
-        :param interval: Frequency of requesting data
-        :param startDate: The start UTC+0 Human Time in type: dd/mm/yy
-        :param endDate: The end Human UTC+0 Time in type: dd/mm/yy (default: now)
+        Get Candles Data From API In An Specific Interval With Limit 1000 Candle
+        :param interval: The interval of an candlestick ("1m","5m","1h","5h","1d","1m","1y")
+        :param startDate: The start UTC+0 Human Time in type: timestamp format
+        :param endDate: The end Human UTC+0 Time in type: timestamp format (default: now)
         :return: return text in json type
         """
-        tmpStartDate = str(int(time.mktime(datetime.datetime.strptime(startDate, "%d/%m/%Y").timetuple()))*1000)
         if endDate == "now":
-            tmpEndDate = str(int(datetime.datetime.now().timestamp())*1000)
-        else:
-            tmpEndDate = str(int(time.mktime(datetime.datetime.strptime(endDate, "%d/%m/%Y").timetuple()))*1000)
+            endDate = str(int(datetime.datetime.now().timestamp())*1000)
         endPoint = END_POINT + "/api/v3/klines"
-        url = endPoint + "?symbol=" + self.symbol + self.eSymbol + "&" +"interval=" + interval +"&startTime=" + tmpStartDate +"&endTime=" + tmpEndDate
+        url = endPoint + "?symbol=" + self.symbol + self.eSymbol + "&" +"interval=" + interval +"&startTime=" + startDate  +"&endTime=" + endDate +"&limit=1000"
         
         payload={}
         headers = {}
 
         response = requests.request("GET", url, headers=headers, data=payload)
-
+        
         return response.text
+
+   
+    def getCandlesticks(self,startDate, endDate="now"):
+        """
+        Get Candles Data From API In An Specific Interval
+        :param interval: The interval of an candlestick ("1m","5m","1h","5h","1d","1m","1y")
+        :param startDate: The start UTC+0 Human Time in type: timestamp format
+        :param endDate: The end Human UTC+0 Time in type: timestamp format (default: now)
+        :return: a json file
+        """
+        if endDate == "now":
+            endDate = str(int(datetime.datetime.now().timestamp())*1000)
+        while startDate<endDate:
+            responseText = self.getCandlesticksWithLimit1000("1m",startDate,endDate)
+            tmp = json.loads(responseText)
+            tmpEndDate = tmp[len(tmp)-1][0]
+            startDate = str(tmpEndDate +60000)
+            if os.path.exists('./data/'+self.symbol+self.eSymbol+".json"): 
+                jsonProcess.deleteLastCharacterInJsonFile(self.symbol+self.eSymbol)
+                jsonProcess.transferDataToJsonFile(","+responseText[1:],self.symbol+self.eSymbol)
+            else:
+                jsonProcess.transferDataToJsonFile(responseText,self.symbol+self.eSymbol)
+
+
     def getRecentTrade(self):
         """
         :return: return new trade in text in json type
@@ -45,3 +73,30 @@ class marketData:
         response = requests.request("GET", url, headers=headers, data=payload)
 
         return response.text
+
+
+
+
+        
+
+### DEVELOPING
+    def realTimeUpdating(self): 
+        tmpEndDate = str(int(datetime.datetime.now().timestamp())*1000)
+        while True: 
+            self.getCandlesticks("1m","")
+### DEVELOPING
+
+
+### Need to comment
+    def testCandleData(self):
+        flag = True
+        ErrorIndex = -1
+        with open ("./data/"+self.symbol+self.eSymbol+".json") as f: 
+            tmp = json.loads(f.read())
+        for i in range(len(tmp)-1):
+            if tmp[i+1][0] - tmp[i][0] != 60000:
+                flag = False
+                ErrorIndex = i
+                break
+        return flag, ErrorIndex
+### Need to comment
