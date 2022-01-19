8556ecead978c301d44c00c2ed8b5c7eaa6ff719
Nhat Vo <tonystark0243@gmail.com>
2022-01-19 17:39:55 +0100

    added checks and fixed naming

--- binancess/market_data.py
+++ binancess/market_data.py
@@ -3,35 +3,49 @@ import requests
 import json
 import os.path
 from requests.models import REDIRECT_STATI
-from toolss import jsonProcess, GoogleDriveConnection as ggdrive
+from toolss import gdrive_connection as gdrive, jsonProcess
 import datetime
-END_POINT = "https://api.binance.com"
+ENDPOINT = "https://api.binance.com"
 class MarketData: 
-    def __init__(self, symbol,eSymbol = "USDT"):
+    def __init__(self, from_symbol,to_symbol = "USDT"):
         """ 
         Source of Data for exchanging rate between a couple of assets(cryto and forex). If there is only one parameter, it is assumed that this will be converted to USD.
-        :param symbol: The symbol of Cryto to be converted
-        :param eSymbol: The symbol of Cryto convert to (default: USD)
+        
+        Parameters:
+            from_symbol: The symbol of Crypto to be converted
+            to_symbol: The symbol of Crypto convert to (default: 'USDT')
         """
-        self.symbol = symbol
-        self.eSymbol = eSymbol
-        self.dataFile = symbol + eSymbol
+        self.from_symbol = from_symbol
+        self.to_symbol = to_symbol
+        self.symbol = from_symbol + to_symbol
+        self.candlesticks_intervals = {
+            'm': 60 * 1000,
+            'h': 3600 * 1000,
+            'd': 24 * 3600 * 1000,
+            'w': 7 * 24 * 3600 * 1000,
+            'M': 30 * 7 * 24 * 3600 * 1000 
+        }
 
 
-    def getCandlesticksWithLimit(self, interval,startDate, limit=1000, endDate="now"):
+    def get_candlesticks_with_limit(self, interval: str,start_time: int, limit: int=1000, end_time: int=-1) -> str:
         """
         Get Candles Data From API In An Specific Interval With Limit 1000 Candle
-        :param interval: The interval of an candlestick ("1m","5m","1h","5h","1d","1m","1y")
-        :param startDate: The start UTC+0 Human Time in type: timestamp format
-        :param endDate: The end Human UTC+0 Time in type: timestamp format (default: now)
-        :param limit: The maximum number of candlesticks
-        :return: return text in json type
+
+        Parameters:
+            interval: The interval of an candlestick (time unit is 'm', 'h', 'd', 'w', 'M')
+                Eg: "1m","5m","1h","5h","1d","1w", "12M"
+            start_time: The start UTC+0 Human Time in type: timestamp format
+            end_time: The end Human UTC+0 Time in type: timestamp format (default: now)
+            limit: The maximum number of candlesticks
+        
+        Return:
+            text in json type
         """
-        if endDate == "now":
-            endDate = int(datetime.datetime.now().timestamp()) * 1000
-        startDate, endDate = str(startDate), str(endDate)
-        endPoint = END_POINT + "/api/v3/klines"
-        url = endPoint + "?symbol=" + self.symbol + self.eSymbol + "&" +"interval=" + interval +"&startTime=" + startDate  +"&endTime=" + endDate +"&limit=" + str(limit)
+        if end_time == -1:
+            end_time = int(datetime.datetime.now().timestamp()) * 1000
+        start_time, end_time = str(start_time), str(end_time)
+        endpoint = ENDPOINT + "/api/v3/klines"
+        url = endpoint + "?symbol=" + self.from_symbol + self.to_symbol + "&" +"interval=" + interval +"&startTime=" + start_time  +"&endTime=" + end_time +"&limit=" + str(limit)
         
         payload={}
         headers = {}
@@ -41,7 +55,7 @@ class MarketData:
         return response.text
 
    
-    def getCandlesticks(self, startDate, endDate=None, interval=None, filename=None):
+    def get_candlesticks(self, start_time, end_time=None, interval=None, filename=None):
         """
         Get Candles Data From API In An Specific Interval and write into the specified filename
         /!\ Delete existing file
@@ -49,53 +63,56 @@ class MarketData:
         Parameters:
             interval: 
                 The interval of an candlestick ("1m","5m","1h","5h","1d","1m","1y")
-            startDate: int
-                The start UTC+0 Human Time in ms in Unix Timestamp
+            start_time: The start UTC+0 Human Time in ms in Unix Timestamp
                 Ex: 1642527663000
-            endDate: int
-                The start UTC+0 Human Time in ms in Unix Timestamp, "now" for now
+            end_time: The start UTC+0 Human Time in ms in Unix Timestamp, "now" for now
                 Ex: 1642527663000, "now"
                 Default: "now"
-            filename: string
-                Name of file to be written into (path: ./data/{filename}.json)
+            filename: Name of file to be written into (path: ./data/{filename}.json)
                 Ex: "BTCUSDT"
-                Default: {self.symbol + self.eSymbol}
+                Default: {self.data_file}
         """
-        if endDate is None:
-            endDate = int(datetime.datetime.now().timestamp()) * 1000
+        if end_time is None:
+            end_time = int(datetime.datetime.now().timestamp()) * 1000
 
         if filename is None:
-            filename = self.symbol + self.eSymbol
+            filename = self.from_symbol + self.to_symbol
 
         if interval is None:
             interval = '1m'
+
+        try:
+            interval_time = int(interval[:-1]) * self.candlesticks_intervals[interval[-1]]
+        except:
+            print('Invalid Interval')
+            return
         
         path = './data/'+filename+".json"
         if os.path.exists(path):
             os.remove(path)
 
-        while startDate < endDate:
-            responseText = self.getCandlesticksWithLimit(interval,startDate,endDate)
-            tmp = json.loads(responseText)
+        while start_time < end_time:
+            response_text = self.get_candlesticks_with_limit(interval,start_time,end_time)
+            tmp = json.loads(response_text)
             
             if len(tmp) == 0: 
                 break
             
-            tmpEndDate = tmp[len(tmp)-1][0]
-            startDate = tmpEndDate + 60000
+            tmp_end_time = tmp[len(tmp)-1][0]
+            start_time = tmp_end_time + interval_time
             if os.path.exists(path): 
-                jsonProcess.deleteLastCharacterInJsonFile(self.symbol+self.eSymbol)
-                jsonProcess.transferDataToJsonFile(","+responseText[1:],self.symbol+self.eSymbol)
+                jsonProcess.deleteLastCharacterInJsonFile(self.from_symbol+self.to_symbol)
+                jsonProcess.transferDataToJsonFile(","+response_text[1:],self.from_symbol+self.to_symbol)
             else:
-                jsonProcess.transferDataToJsonFile(responseText,self.symbol+self.eSymbol)
+                jsonProcess.transferDataToJsonFile(response_text,self.from_symbol+self.to_symbol)
 
 
     def getRecentTrade(self):
         """
-        :return: return new trade in text in json type
+        Return new trade in text in json type
         """
-        endPoint = END_POINT + "/api/v3/trades"
-        url = endPoint + "?symbol=" + self.symbol + self.eSymbol 
+        endPoint = ENDPOINT + "/api/v3/trades"
+        url = endPoint + "?symbol=" + self.from_symbol + self.to_symbol 
         
         payload={}
         headers = {}
@@ -105,18 +122,21 @@ class MarketData:
         return response.text
 
 
-    def upload_old_data(self):
+    def upload_old_data(self) -> None:
+        """
+        Upload old data of this market to drive
+        """
         interval = 60 * 1000
         end_time = int(datetime.datetime.now().timestamp()) * 1000
         
-        resp = json.loads(self.getCandlesticksWithLimit('1m', 0, 5))
+        resp = json.loads(self.get_candlesticks_with_limit('1m', 0, 5))
         if len(resp) == 0:
             return
         start_time = resp[0][0] // interval * interval
 
         while start_time < end_time:
             # print(start_time)
-            exists = ggdrive.get_file(start_time, self.dataFile)
+            exists = gdrive.get_file(start_time, self.symbol)
             if exists is not None:
                 exists = json.loads(exists.GetContentString())
                 if len(exists) != 0:
@@ -126,13 +146,13 @@ class MarketData:
                     start_time += 60 * 60 * 1000
                 continue
 
-            resp = self.getCandlesticksWithLimit('1m', start_time, 60, end_time)
+            resp = self.get_candlesticks_with_limit('1m', start_time, 60, end_time)
             body = json.loads(resp)
 
             if len(body) == 0:
                 break
 
-            ggdrive.upload_to_drive(start_time, self.dataFile, resp)
+            gdrive.upload_to_drive(start_time, self.symbol, resp)
 
             start_time = body[-1][0] + 60000
 
@@ -142,7 +162,7 @@ class MarketData:
     def realTimeUpdating(self): 
         tmpEndDate = str(int(datetime.datetime.now().timestamp())*1000)
         while True: 
-            self.getCandlesticks("1m","")
+            self.get_candlesticks("1m","")
 ### DEVELOPING
 
 
@@ -150,7 +170,7 @@ class MarketData:
     def testCandleData(self):
         flag = True
         ErrorIndex = -1
-        with open ("./data/"+self.symbol+self.eSymbol+".json") as f: 
+        with open ("./data/"+self.from_symbol+self.to_symbol+".json") as f: 
             tmp = json.loads(f.read())
         for i in range(len(tmp)-1):
             if tmp[i+1][0] - tmp[i][0] != 60000:
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from mimetypes import MimeTypes
 from turtle import title
-from binancess.MarketData import MarketData
+from binancess.market_data import MarketData
 from toolss import jsonProcess
 import json
 import csv
-from toolss import GoogleDriveConnection
+from toolss import gdrive_connection
 
 
 data_source = MarketData("ADA")
-GoogleDriveConnection.upload_to_drive(1642511485000,"ADAUSDT",data_source.getCandlesticksWithLimit("1m","1642511485000",5))
+# GoogleDriveConnection.upload_to_drive(1642511485000,"ADAUSDT",data_source.getCandlesticksWithLimit("1m","1642511485000",5))
+data_source.upload_old_data()
