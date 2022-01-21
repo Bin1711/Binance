e74af6e243a99746392d2e0b4d8b74bc77fcc07a
Nhat Vo <tonystark0243@gmail.com>
2022-01-21 11:05:11 +0100

    added start_time arg to upload_old_data

--- binancess/market_data.py
+++ binancess/market_data.py
@@ -122,16 +122,18 @@ class MarketData:
         return response.text
 
 
-    def upload_old_data(self) -> None:
+    def upload_old_data(self, start_time: int=0) -> None:
         """
         Upload old data of this market to drive
         """
         interval = 60 * 1000
         end_time = int(datetime.datetime.now().timestamp()) * 1000
         
+        
         resp = json.loads(self.get_candlesticks_with_limit('1m', 0, end_time, 60))
         if len(resp) == 0:
-            return
+            return            
+        
         start_time = resp[0][0] // interval * interval
 
         while start_time < end_time:
@@ -147,9 +149,10 @@ class MarketData:
             body = json.loads(resp)
 
             if len(body) == 0:
+                print(f'empty response at {start_time}, please try again later')
                 break
 
-            gdrive.upload_to_drive(start_time, self.symbol, resp)
+            gdrive.upload_to_drive(body[0][0], self.symbol, resp)
 
             start_time = body[-1][0] + interval
 
--- binancess/upload.py
+++ binancess/upload.py
@@ -6,15 +6,15 @@ __all__ = ['upload_old_data', 'upload_current_data']
 
 markets = {c: MarketData(c) for c in const.COINS}
 
-def upload_old_data():
+def upload_old_data(start_time: int=0) -> None:
     """
     Upload old data of all markets in const.COINS to drive
     """
     for m in markets.values():
-        m.upload_old_data()
+        m.upload_old_data(start_time)
 
 
-def upload_current_data():
+def upload_current_data() -> None:
     """
     Upload data of all markets in const.COINS for the last 60 mins to drive
     """
