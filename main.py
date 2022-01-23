e4cf3b3da4f9a549f77219be9da5f2b090275f3b
Nhat Vo <tonystark0243@gmail.com>
2022-01-23 17:21:20 +0100

    round upload time by hour

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from turtle import title
 from toolss import jsonProcess
 import json
 import csv
-from toolss import gdrive_connection
+from toolss import gdrive
 from binancess import upload, market_data
 
 # GoogleDriveConnection.upload_to_drive(1642511485000,"ADAUSDT",data_source.getCandlesticksWithLimit("1m","1642511485000",5))
