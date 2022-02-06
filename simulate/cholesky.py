ffa9e69fc41f28b5aadecd0d0059469c2c8c6265
b1n1234567 <binn171100@gmail.com>
2022-02-06 12:00:44 -0600

    75%

--- simulate/cholesky.py
+++ simulate/cholesky.py
@@ -1,43 +1,5 @@
 import numpy as np
 import pandas as pd
-import json
-# END_POINT = "https://api.binance.us"
-
-# def close_column(file):
-#     f = open(file)
-#     data = json.load(f)
-#     s = []
-#     for i in data:
-#         s.append(float(i[4]))
-#     f.close()
-#     return s
-
-# def covariance(x, y):
-#     mean_x = sum(x)/float(len(x))
-#     mean_y = sum(y)/float(len(y))
-#     sub_x = [i - mean_x for i in x]
-#     sub_y = [i - mean_y for i in y]
-#     numerator = sum([sub_x[i] * sub_y[i] for i in range(len(sub_x))])
-#     denominator = len(x) - 1
-#     cov = numerator / denominator
-#     return cov
-
-# def get_cov_into_matrix(name): #sorry, this should be corr instead of cov
-#     s = []
-#     for i in name:
-#         s.append(close_column(i))
-#     t = np.array([])
-#     for i in s:
-#         u = np.array([])
-#         for j in s:
-#             u = np.append(u, covariance(i , j))
-#         t = np.concatenate((t, np.array(u)))
-#     return t
-
-# def get_cholesky(name):
-#     arr = get_cov_into_matrix(name)
-#     L = np.linalg.cholesky(arr)
-#     return L
 
 def cholesky(data):
     # Thanh's version
@@ -58,6 +20,32 @@ def cholesky(data):
     returns_df = pd.DataFrame(columns = data.keys())
     for sym in data.keys():
         returns_df[sym] = data[sym]['close'].pct_change()
+    
+    # compute correlation matrix
+    cov_matrix = returns_df.corr()
+    
+    # compute cholesky matrix
+    cholesky = np.linalg.cholesky(cov_matrix)
+    return cholesky
+
+def cholesky2(data):
+    # Thanh's version
+    """
+    This function computes cholesky matrix just for open and close
+
+    Parameters
+    ----------
+    data : Dataframe
+    
+    Returns
+    -------
+    cholesky : np.array
+        This is cholesky matrix.
+    """
+    # create new dataframe contains returns of all symbols
+    returns_df = pd.DataFrame(columns = ['open', 'close'])
+    for sym in ['open', 'close']:
+        returns_df[sym] = data[sym].pct_change()
     
     # compute correlation matrix
     cov_matrix = returns_df.corr()
---
+++ simulate/simulate_multiple_time_series.py
@@ -0,0 +1,85 @@
+import pandas as pd
+import numpy as np
+from torch import var
+import cholesky
+from pandas import DataFrame, read_csv
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
+from math import sqrt
+import simulatedata
+
+def simulate_open_price_and_close (data):
+    """This function will simulate 2 series: open price and close price of 1 symbol at the same time
+
+    Params:
+        data : Dataframe of a data contains open and close column 
+
+    Returns:
+        simulated data
+    """
+    var_close = compute_std(data['close'])
+    mean_close = data['close'].mean()
+    var_open = compute_std(data['open'])
+    mean_open = data['open'].mean()
+    chol = cholesky.cholesky2(data)
+    data1 = pd.DataFrame(data, columns = ['time', 'open', 'close'], index='time')
+    data1['open'] = normalize_or_standardize_data(data['open'].pct_change(), is_normalize= False) #check if we need pact_change()
+    data1['close'] = normalize_or_standardize_data(data['open'].pct_change(), is_normalize= False)
+    simulate_corr_rets = pd.DataFrame(np.matmul(chol, data1), index =['open', 'close']).T/100
+    res = pd.DataFrame(simulate_corr_rets, columns = ['time', 'open', 'close'], index='time')
+    res['open'] = mulback_cholesky(simulate_corr_rets['open',  False, var_open, mean_open ])
+    res['close'] = mulback_cholesky(simulate_corr_rets['open', False, var_close,  mean_close ])
+    t1 = simulatedata.construct_price_series(res['close'],data['close'][0], data.index[0]) # this and the line below will only be used if we use pct_change above
+    t2= simulatedata.construct_price_series(res['open'], data['open'][0], data.index[0])
+    res['close'] = t1['close']
+    res['open'] = t2['open']
+    return res
+
+def compute_std (data):
+    # this is just for compute the variance of data
+    return sqrt(np.var(data))
+
+def normalize_or_standardize_data(data, is_normalize = True):
+    """Normalize data
+
+    Params:
+        data: dataframe
+        is_normalize: Default: True: normalize data
+                      Fault: standardize
+    """
+    if is_normalize:
+        # y = (x - min) / (max - min)
+        data = data.reshape((len(data), 1))
+        scaler = MinMaxScaler(feature_range=(0, 1))
+        scaler = scaler.fit(data)
+        res = scaler.transform(data)
+        return res
+    else:
+        # y = (x - mean)/standard deviation
+        data = data.reshape((len(data), 1))
+        scaler = StandardScaler()
+        scaler = scaler.fit(data)
+        res = scaler.transform(data)
+        return res
+        
+    
+def mulback_cholesky (data, is_normalize = True, x1 =0, x2 = 0):
+    """This function is to inverse of nomalization
+
+    Params:
+        data: dataframe
+        is_normalize: Default: True: apply for normalize data, then x1 = min, x2 = range
+                      Fault: aplly for standardize, then x1 = mean, x2 = std
+    """
+    if is_normalize:
+        range = x2
+        min = x1
+        data1 = data.multiply(range)
+        data1 = data1.add(min)
+        return data1
+    else:
+        std = x2
+        mean = x1
+        data1 = data.multiply(std)
+        data1 = data1.add(mean)
+        return data1
