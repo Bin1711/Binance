bf8d66682a09278baa45e45aa6686a5201f7d29a
b1n1234567 <binn171100@gmail.com>
2022-02-08 10:30:47 -0600

    .

--- simulate/simulate_multiple_time_series.py
+++ simulate/simulate_multiple_time_series.py
@@ -1,12 +1,13 @@
+from black import T
 import pandas as pd
 import numpy as np
-from torch import var
-import cholesky
+from torch import t, var
+from simulate import cholesky
 from pandas import DataFrame, read_csv
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import StandardScaler
 from math import sqrt
-import simulatedata
+
 
 def simulate_open_price_and_close (data):
     """This function will simulate 2 series: open price and close price of 1 symbol at the same time
@@ -22,17 +23,22 @@ def simulate_open_price_and_close (data):
     var_open = compute_std(data['open'])
     mean_open = data['open'].mean()
     chol = cholesky.cholesky2(data)
-    data1 = pd.DataFrame(data, columns = ['time', 'open', 'close'], index='time')
-    data1['open'] = normalize_or_standardize_data(data['open'], is_normalize= False) 
-    data1['close'] = normalize_or_standardize_data(data['open'], is_normalize= False)
-    simulate_corr_rets = pd.DataFrame(np.matmul(chol, data1), index =['open', 'close']).T/100
-    res = pd.DataFrame(simulate_corr_rets, columns = ['time', 'open', 'close'], index='time')
-    res['open'] = mulback_cholesky(simulate_corr_rets['open',  False, var_open, mean_open])
-    res['close'] = mulback_cholesky(simulate_corr_rets['open', False, var_close,  mean_close])
-    return res
+    data1 = []
+    data1 = np.append(data1, normalize_or_standardize_data(data['open'], is_normalize= False))
+    data1 = np.append(data1, normalize_or_standardize_data(data['close'], is_normalize= False))
+    data1 = data1.reshape(len(data['close']), 2)
+    # data1['open'] = normalize_or_standardize_data(data['open'], is_normalize= False) 
+    # data1['close'] = normalize_or_standardize_data(data['close'], is_normalize= False)
+    data1 = np.transpose(np.matmul(data1, chol))
+    simulate_corr_rets = pd.DataFrame(data)
+    simulate_corr_rets['close'] = data1[1]
+    simulate_corr_rets['open'] = data1[0]
+    simulate_corr_rets['open'] = mulback_cholesky(simulate_corr_rets['open'],  False, var_open, mean_open)
+    simulate_corr_rets['close'] = mulback_cholesky(simulate_corr_rets['close'], False, var_close,  mean_close)
+    return simulate_corr_rets
 
 def compute_std (data):
-    # this is just for compute the variance of data
+    # this is just for compute the std of data
     return sqrt(np.var(data))
 
 def normalize_or_standardize_data(data, is_normalize = True):
@@ -43,19 +49,26 @@ def normalize_or_standardize_data(data, is_normalize = True):
         is_normalize: Default: True: normalize data
                       Fault: standardize
     """
+    std = compute_std(data)
+    mean = data.mean()
     if is_normalize:
         # y = (x - min) / (max - min)
-        data = data.reshape((len(data), 1))
+        data = data.values.resize((0, 2))
+        data = data.reshape((-1, 1))
         scaler = MinMaxScaler(feature_range=(0, 1))
         scaler = scaler.fit(data)
         res = scaler.transform(data)
         return res
     else:
         # y = (x - mean)/standard deviation
-        data = data.reshape((len(data), 1))
-        scaler = StandardScaler()
-        scaler = scaler.fit(data)
-        res = scaler.transform(data)
+        # values = data.values
+        # values = values.reshape((len(data), 1))
+        # scaler = StandardScaler()
+        # scaler = scaler.fit(values)
+        # res = scaler.transform(values)
+        res = []
+        for i, j in enumerate(data):
+            res.append((j - mean)/ std)
         return res
         
     
